000fc4b72513cbd964e3be9bf984ba9a83eb418e
hanhan258 <96119846+hanhan258@users.noreply.github.com>
2023-03-28 11:32:10 +0800

    私聊时不再@发送者

--- 憨憨小功能.js
+++ 憨憨小功能.js
@@ -13,7 +13,7 @@ export class jiami extends plugin {
 			/** https://oicqjs.github.io/oicq/#events */
 			event: 'message',
 			/** 优先级，数字越小等级越高 */
-			priority: 6000,
+			priority: 6,
 			rule: [
 				{
 					/** 命令正则匹配 */
