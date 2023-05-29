e9157b1b418d57105867713d240bfaae25f3b003
sunsetAlaoneShine <133138792+sunsetAlaoneShine@users.noreply.github.com>
2023-05-29 13:12:37 +0800

    fix: 不再检测动图

--- 自动图片审查.js
+++ 自动图片审查.js
@@ -7,9 +7,7 @@ import https from 'https'
 
 /**
  * 需要安装依赖``` pnpm i jimp jsqr @tensorflow/tfjs-node nsfwjs -w ```
- * 一个很简单的插件，一看就是有手就行，没手也行。本来就是看现成的插件满足不了我的需求，做个给自己用罢了。你一定可以写一个比我这个更好的，有现成的插件的话我就不用浪费时间做这个了。
- * 这个插件默认会从nsfwjs库官方提供的S3对象存储中load()模型，模型很小，量化模型只有5.94MB，但仍不能保证网络始终稳定。我试了一下nodejs18本地加载模型失败了，欢迎大佬提pr解决一下本地加载模型的问题。
- * 本插件明确存在内存泄漏现象，您应该自己思考是否使用这个插件。可以尝试将config/pm2/pm2.json文件的max_memory_restart字段的值改成1G，例如"max_memory_restart": "1G"，小鸡可能还需要修改一下内核参数。或许我们应该白嫖huggingface的算力。
+ * 一个很简单的插件，是在无聊的生活中增加的一个无意义的功能。
  */
 
 //是否自动撤回nsfw图片，true为真，false为假，默认不撤回主人。
@@ -18,7 +16,7 @@ const recall = false
 //是否自动撤回二维码图片，true为真，false为假，默认不撤回主人。开启后默认不会发送解码内容哦。
 const recallQR = false
 
-//涩图转发，监听到nsfw图片后转发给预设QQ号或者群号。postMethod可选的值为private和group，前者表示私聊发送，后者群聊发送。postNum填需要通知的QQ号或者群号，留空则关闭此功能。
+//涩图转发，监听到nsfw图片后转发给预设QQ号或者群号。postMethod可选的值为private或者group，前者表示私聊发送，后者群聊发送。postNum填需要通知的QQ号或者群号，留空则关闭此功能。
 const postMethod = 'group'
 const postNum = [] //虽然用的是数组，但是最多只能输入一个号码
 
@@ -64,16 +62,17 @@ export class autoCheck extends plugin {
     // }
     // const imageUrl = this.e.message[0].url
 
-    const imageUrl = this.e.message.find(msg => msg.type === 'image')?.url || null
-    if (!imageUrl) {
+    const image = this.e.message.find(msg => msg.type === 'image') || null
+    if (!image?.url || image.file.endsWith('.gif')) {
       return false
     }
+    const imageUrl = image.url
     const regex = /-(\w{32})\//
     const hash = imageUrl.match(regex)[1]
 
     if (await redis.exists(`Yz:autoCheck:${hash}`)) {
       logger.info('[图片审查]图片安全，重置缓存时间')
-      await redis.expire(`Yz:autoCheck:${hash}`, 48 * 60 * 60)
+      await redis.expire(`Yz:autoCheck:${hash}`, 72 * 60 * 60)
       return false
     } else if (await redis.exists('Yz:autoCheckLock')) {
       logger.info('[图片审查]当前队列存在待处理图片')
@@ -90,7 +89,7 @@ export class autoCheck extends plugin {
     } else {
       logger.info('[图片审查]图片安全')
       await redis.del('Yz:autoCheckLock')
-      await redis.set(`Yz:autoCheck:${hash}`, '0', { EX: 36 * 60 * 60 })
+      await redis.set(`Yz:autoCheck:${hash}`, '0', { EX: 48 * 60 * 60 })
       return false
     }
   }
