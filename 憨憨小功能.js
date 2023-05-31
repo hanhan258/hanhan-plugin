3b3d1a97db8c576a387b95c955f06457faac5d64
Sean <1519059137@qq.com>
2023-05-31 13:05:08 +0800

    refactor： 1. 移除“ping”功能中域名转ip的api，使用node自带工具(dns.lookup)实现转换。 2. 使用curl代替fetch以获取本地代理信息 3. 支持查询本地ip信息

--- 憨憨小功能.js
+++ 憨憨小功能.js
@@ -70,7 +70,7 @@ export class jiami extends plugin {
         },
         {
           /** 命令正则匹配 */
-          reg: '^#?ping ',
+          reg: '^#?ping',
           /** 执行方法 */
           fnc: 'ping'
         },
@@ -247,63 +247,102 @@ export class jiami extends plugin {
   // ping网站或ip
   async ping (e) {
     if (!token) return false
-    let ipAddress = e.msg.trim().match(/^#?ping\s?(.+)/)[1]
-    if (!ipAddress) {
+    let host = e.msg.trim().replace(/^#?ping\s?/, '').replace(/https?:\/\//, '')
+    if (!host) {
       await this.reply('请检查输入是否有误~', e.isGroup)
       return false
     }
     await this.reply('在ping了、在ping了。。。', true, { recallMsg: e.isGroup ? 3 : 0 })
     const regex = /^(([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,6}:|([0-9a-fA-F]{1,4}:){1,5}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,4}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,5}|:)|:((:[0-9a-fA-F]{1,4}){1,6}|:))%?([0-9a-zA-Z]{1,})?$/
-    // 获取公网ip
-    if (!regex.test(ipAddress)) {
-      function lookup (hostname) {
-        return new Promise((resolve, reject) => {
-          dns.lookup(hostname, (err, address) => {
-            if (err) {
-              reject(err)
+    let pingCommand = ''
+    let pingRegex = ''
+    let pingRes = ''
+    // 不ping本机
+    if (host !== 'me') {
+      // 获取公网ip
+      if (!regex.test(host)) {
+        function lookup (hostname) {
+          return new Promise((resolve, reject) => {
+            dns.lookup(hostname, (err, address) => {
+              if (err) {
+                reject(err)
+              } else {
+                resolve(address)
+              }
+            })
+          })
+        }
+        try {
+          host = await lookup(host)
+        } catch (err) {
+          await this.reply(err.toString(), e.isGroup)
+          logger.error('ipAddress err: ', err)
+          return false
+        }
+      }
+
+      if (process.platform === 'win32') {
+        // 设置控制台的编码格式
+        execSync('chcp 65001')
+        // 设置ping指令
+        pingCommand = `ping -n 4 ${host}`
+        // 设置处理ping结果的正则表达式
+        pingRegex = /Packets: Sent = (\d+), Received = (\d+), Lost = \d+ \((\d+%) loss\),\s*\nApproximate round trip times in milli-seconds:\s*\n\s*Minimum = (\d+)ms, Maximum = (\d+)ms, Average = (\d+)ms/
+      } else {
+        process.env.LANG = 'en_US.UTF-8'
+        pingCommand = `ping -c 4 ${host}`
+        pingRegex = /(\d+\.?\d*) packets transmitted, (\d+\.?\d*) received, (\d+\.?\d*%) packet loss, time \d+ms\s*\n\s*rtt min\/avg\/max\/mdev = (\d+\.?\d*)\/(\d+\.?\d*)\/(\d+\.?\d*)\/\d+\.?\d* ms/
+      }
+      try {
+        // 获取ping结果
+        pingRes = await new Promise((resolve, reject) => {
+          exec(pingCommand, async (error, stdout, stderr) => {
+            if (error) {
+              reject(error)
             } else {
-              resolve(address)
+              resolve(stdout)
             }
           })
         })
+      } catch (error) {
+        logger.error(`exec pingCommand执行出错: ${error}`)
       }
-      try {
-        ipAddress = await lookup(ipAddress)
-      } catch (err) {
-        await this.reply(err.toString(), e.isGroup)
-        logger.error(err)
-        return false
+      const match = pingRegex.exec(pingRes.toString())
+      if (match) {
+        const packetCount = match[1]
+        const receivedCount = match[2]
+        const lossRate = match[3]
+        const minDelay = match[4]
+        const maxDelay = match[5]
+        const avgDelay = match[6]
+        pingRes = `最小延迟：${minDelay}ms\n最大延迟：${maxDelay}ms\n平均延迟：${avgDelay}ms\n数据包数：${packetCount}\n接受数据包：${receivedCount}\n丢包率：${lossRate}`
+      } else {
+        pingRes = pingRes === '' ? `目标地址${!e.isGroup ? '(' + e.msg.trim().match(/^#?ping\s?(.+)/)[1] + ')' : ''}无法响应，请检查网络连接是否正常(是否需要代理访问？)，或者该站点是否已关闭。` : pingRes.replace(new RegExp(host, 'g'), '=͟͟͞͞ʕ•̫͡•ʔ=͟͟͞͞ʕ•̫͡•ʔ=͟͟͞͞ʕ•̫͡•ʔ')
       }
     }
-    let pingCommand = ''
-    if (process.platform === 'win32') {
-      // 设置控制台的编码格式
-      execSync('chcp 65001')
-      pingCommand = `ping -n 4 ${ipAddress}`
-    } else {
-      process.env.LANG = 'en_US.UTF-8'
-      pingCommand = `ping -c 4 ${ipAddress}`
-    }
-    // 获取ping结果
-    let pingRes = await new Promise((resolve, reject) => {
-      exec(pingCommand, (error, stdout, stderr) => {
-        if (error) {
-          logger.error(`exec执行出错: ${error}`)
-          reject(error)
-        } else {
-          resolve(stdout)
-        }
+    let ipInfo = ''
+    try {
+      // 通过ipinfo.io获取ip地址相关信息
+      ipInfo = await new Promise((resolve, reject) => {
+        exec(`curl https://ipinfo.io/${host === 'me' ? '' : host}?token=${token}`, async (error, stdout, stderr) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(stdout)
+          }
+        })
       })
-    })
-    // 替换ip地址
-    pingRes = pingRes.replace(new RegExp(ipAddress, 'g'), '=͟͟͞͞ʕ•̫͡•ʔ=͟͟͞͞ʕ•̫͡•ʔ=͟͟͞͞ʕ•̫͡•ʔ')
-    // 获取ip信息
-    let ipInfoRes = await fetch(`https://ipinfo.io/${ipAddress}?token=${token}`)
-    let ipInfo = await ipInfoRes.json()
-    let res = `国家代码：${ipInfo.country}\n地区：${ipInfo.region}\n城市：${ipInfo.city}\n经纬度：${ipInfo.loc}\n运营商：${ipInfo.org}\n时区：${ipInfo.timezone}\n${pingRes}`
+    } catch (error) {
+      logger.error(`exec curl执行出错: ${error}`)
+      await this.reply(error, e.isGroup)
+      return false
+    }
+    ipInfo = JSON.parse(ipInfo.trim())
+    let res = `${!e.isGroup ? 'IP: ' + ipInfo.ip + '\n' : ''}国家：${ipInfo.country}\n地区：${ipInfo.region}\n城市：${ipInfo.city}\n时区：${ipInfo.timezone}\n经纬度：${ipInfo.loc}\n运营商：${ipInfo.org}\n${pingRes}`
     await this.reply(res, e.isGroup)
     return true
   }
+
   //莫斯加密
   async morseEn(e) {
     let sendmsg = [];
