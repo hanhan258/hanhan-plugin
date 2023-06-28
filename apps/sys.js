290b5ea0ee317fcb667b9364eb0328284fdf236c
ycxom <113200758+ycxom@users.noreply.github.com>
2023-06-28 21:14:29 +0800

    修改正则

--- apps/sys.js
+++ apps/sys.js
@@ -14,163 +14,163 @@ export class sys extends plugin {
       priority: 50,
       rule: [
         {
-          reg: '^#?(百度|baidu)(=|＝)?(.*)$',
+          reg: '^#(百度|baidu)(=|＝)?(.*)$',
           fnc: 'so_baidu'
         },
         {
-          reg: '^#?搜狗(=|＝)?(.*)$',
+          reg: '^#搜狗(=|＝)?(.*)$',
           fnc: 'so_sg'
         },
         {
-          reg: '^#?duckduckgo(=|＝)?(.*)$',
+          reg: '^#duckduckgo(=|＝)?(.*)$',
           fnc: 'so_duckduckgo'
         },
         {
-          reg: '^#?必应(=|＝)?(.*)$',
+          reg: '^#必应(=|＝)?(.*)$',
           fnc: 'so_bing'
         },
         {
-          reg: '^#?google(=|＝)?(.*)$',
+          reg: '^#google(=|＝)?(.*)$',
           fnc: 'so_google'
         },
         {
-          reg: '^#?wiki(=|＝)?(.*)$',
+          reg: '^#wiki(=|＝)?(.*)$',
           fnc: 'so_wiki'
         },
         {
-          reg: '^#?ecosia(=|＝)?(.*)$',
+          reg: '^#ecosia(=|＝)?(.*)$',
           fnc: 'so_ecosia'
         },
         {
-          reg: '^#?360(=|＝)?(.*)$',
+          reg: '^#360(=|＝)?(.*)$',
           fnc: 'so_360'
         },
         {
-          reg: '^#?2345(=|＝)?(.*)$',
+          reg: '^#2345(=|＝)?(.*)$',
           fnc: 'so_2345'
         },
         {
-          reg: '^#?youtube(=|＝)?(.*)$',
+          reg: '^#youtube(=|＝)?(.*)$',
           fnc: 'so_ytb'
         },
         {
-          reg: '^#?推特找人(=|＝)?(.*)$',
+          reg: '^#推特找人(=|＝)?(.*)$',
           fnc: 'so_twzr'
         },
         {
-          reg: '^#?科学打开网页(=|＝)?(.*)$',
+          reg: '^#科学打开网页(=|＝)?(.*)$',
           fnc: 'so_fqopenwebui'
         },
         {
-          reg: '^#?打开网页(=|＝)?(.*)$',
+          reg: '^#打开网页(=|＝)?(.*)$',
           fnc: 'so_openwebui'
         },
         {
-          reg: '^#?bilibili(=|＝)?(.*)$',
+          reg: '^#bilibili(=|＝)?(.*)$',
           fnc: 'so_blbl'
         },
         {
-          reg: '^#?github(=|＝)?(.*)$',
+          reg: '^#github(=|＝)?(.*)$',
           fnc: 'so_github'
         },
         {
-          reg: '^#?动漫资源(=|＝)?(.*)$',
+          reg: '^#动漫资源(=|＝)?(.*)$',
           fnc: 'so_acg'
         },
         {
-          reg: '^#?cnping(=|＝)?(.*)$',
+          reg: '^#cnping(=|＝)?(.*)$',
           fnc: 'so_ping'
         },
         {
-          reg: '^#?webcrawler(=|＝)?(.*)$',
+          reg: '^#webcrawler(=|＝)?(.*)$',
           fnc: 'so_webcrawler'
         },
         {
-          reg: '^#?aol(=|＝)?(.*)$',
+          reg: '^#aol(=|＝)?(.*)$',
           fnc: 'so_aol'
         },
         {
-          reg: '^#?ask(=|＝)?(.*)$',
+          reg: '^#ask(=|＝)?(.*)$',
           fnc: 'so_ask'
         },
         {
-          reg: '^#?yahoo(=|＝)?(.*)$',
+          reg: '^#yahoo(=|＝)?(.*)$',
           fnc: 'so_yahoo'
         },
         {
-          reg: '^#?pornhub(=|＝)?(.*)$',
+          reg: '^#pornhub(=|＝)?(.*)$',
           fnc: 'so_ph'
         },
         {
-          reg: '^#?pixiv(=|＝)?(.*)$',
+          reg: '^#pixiv(=|＝)?(.*)$',
           fnc: 'so_pixiv'
         },
         {
-          reg: '^#?sankaku(=|＝)?(.*)$',
+          reg: '^#sankaku(=|＝)?(.*)$',
           fnc: 'so_sankaku'
         },
         {
-          reg: '^#?亚马逊(=|＝)?(.*)$',
+          reg: '^#亚马逊(=|＝)?(.*)$',
           fnc: 'so_amz'
         },
         {
-          reg: '^#?niconico(=|＝)?(.*)$',
+          reg: '^#niconico(=|＝)?(.*)$',
           fnc: 'so_niconico'
         },
         {
-          reg: '^#?syosetu(=|＝)?(.*)$',
+          reg: '^#syosetu(=|＝)?(.*)$',
           fnc: 'so_syosetu'
         },
         {
-          reg: '^#?dmm(=|＝)?(.*)$',
+          reg: '^#dmm(=|＝)?(.*)$',
           fnc: 'so_dmm'
         },
         {
-          reg: '^#?cpu排行(.*)$',
+          reg: '^#cpu排行(.*)$',
           fnc: 'so_cpuz'
         },
         {
-          reg: '^#?cpu(=|＝)?(.*)$',
+          reg: '^#cpu(=|＝)?(.*)$',
           fnc: 'so_cpu'
         },
         {
-          reg: '^#?gpu排行',
+          reg: '^#gpu排行',
           fnc: 'so_gpuz'
         },
         {
-          reg: '^#?gpu(=|＝)?(.*)$',
+          reg: '^#gpu(=|＝)?(.*)$',
           fnc: 'so_gpu'
         },
         {
-          reg: '^#?IMDB(=|＝)?(.*)$',
+          reg: '^#IMDB(=|＝)?(.*)$',
           fnc: 'so_imdb'
         },
         {
-          reg: '^#?查ip=$',
+          reg: '^#查ip=$',
           fnc: 'so_ckip'
         },
         {
-          reg: '^#?查ip(=|＝)?(.*)$',
+          reg: '^#查ip(=|＝)?(.*)$',
           fnc: 'so_cip'
         },
         {
-          reg: '^#?wallhere(=|＝)?(.*)$',
+          reg: '^#wallhere(=|＝)?(.*)$',
           fnc: 'so_wallhere'
         },
         {
-          reg: '^#?天眼查(=|＝)?(.*)$',
+          reg: '^#天眼查(=|＝)?(.*)$',
           fnc: 'so_tyc'
         },
         {
-          reg: '^#?steam(=|＝)?(.*)$',
+          reg: '^#steam(=|＝)?(.*)$',
           fnc: 'so_steam'
         },
         {
-          reg: '^#?yandex(=|＝)?(.*)$',
+          reg: '^#yandex(=|＝)?(.*)$',
           fnc: 'so_yandex'
         },
         {
-          reg: '^#?台风路径$',
+          reg: '^#台风路径$',
           fnc: 'so_tf'
         },
       ]
@@ -187,7 +187,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?google(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#google(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -203,7 +203,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?(百度|baidu)(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#(百度|baidu)(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let Timeout = 5000;
       let coverUrl = `https://www.baidu.com/s?wd=${msg}`
@@ -218,7 +218,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?必应(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#必应(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -234,7 +234,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?360(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#360(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let Timeout = 5000;
       let coverUrl = `https://www.so.com/s?q=${msg}`
@@ -249,7 +249,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?推特找人(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#推特找人(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -265,7 +265,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?youtube(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#youtube(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -281,7 +281,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?搜狗(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#搜狗(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let Timeout = 5000;
       let coverUrl = `https://www.sogou.com/web?query=${msg}`
@@ -296,7 +296,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?duckduckgo(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#duckduckgo(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -312,7 +312,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?wiki(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#wiki(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -328,7 +328,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?ecosia(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#ecosia(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -344,7 +344,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?科学打开网页(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#科学打开网页(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -360,7 +360,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?打开网页(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#打开网页(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let Timeout = 5000;
       let coverUrl = `${msg}`
@@ -375,7 +375,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?bilibili(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#bilibili(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let Timeout = 5000;
       let coverUrl = `https://search.bilibili.com/all?keyword=${msg}`
@@ -390,7 +390,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?github(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#github(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -406,7 +406,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?动漫资源(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#动漫资源(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -422,7 +422,7 @@ export class sys extends plugin {
     try {
       await this.reply(`ping中....请等待30秒`);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?cnping(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#cnping(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let Timeout = 33000;
       let coverUrl = `https://acg.rip/?term=${msg}`
@@ -437,7 +437,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?webcrawler(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#webcrawler(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -453,7 +453,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?aol(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#aol(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -469,7 +469,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?ask(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#ask(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -485,7 +485,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?yahoo(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#yahoo(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -501,7 +501,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?pornhub(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#pornhub(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -517,7 +517,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?pixiv(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#pixiv(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -533,7 +533,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?sankaku(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#sankaku(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -549,7 +549,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?亚马逊(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#亚马逊(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -565,7 +565,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?niconico(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#niconico(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -581,7 +581,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?syosetu(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#syosetu(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -597,7 +597,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?dmm(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#dmm(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -627,7 +627,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?cpu(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#cpu(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -657,7 +657,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?cpu(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#cpu(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -673,7 +673,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?IMDB(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#IMDB(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -689,7 +689,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?查ip(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#查ip(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -706,7 +706,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?wallhere(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#wallhere(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -722,7 +722,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?天眼查(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#天眼查(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let Timeout = 5000;
       let coverUrl = `https://www.tianyancha.com/search?key=${msg}`
@@ -737,7 +737,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?steam(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#steam(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
@@ -753,7 +753,7 @@ export class sys extends plugin {
     try {
       await this.reply(this.echo);
       logger.info("[用户命令]", e.msg);
-      let msg = e.msg.replace(/^#?yandex(=|＝)?/, "").trim();
+      let msg = e.msg.replace(/^#yandex(=|＝)?/, "").trim();
       msg = msg.split(" ");
       let proxyUrl0 = this.proxyUrl
       let Timeout = 5000;
