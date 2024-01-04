f72ff6a7fcd900627a9badf5184d7833a0c5dffe
岩浆 <linboyan5438081@163.com>
2024-01-05 03:10:38 +0800

    优化俄罗斯转盘在QQBot下的表现

--- apps/els.js
+++ apps/els.js
@@ -36,7 +36,7 @@ export class RussiaRoundPlatePlugin extends plugin {
     let groupId = e.group_id
     let groupLock = await redis.get(`HANHAN:ELS:${groupId}`)
     if (!groupLock) {
-      let totalBullets = 6
+      let totalBullets = 6 //尊重左轮手枪装弹量
       let realBullets = Math.floor(Math.random() * totalBullets)
       let bulletNum = JSON.stringify([totalBullets, realBullets])
       await redis.set(`HANHAN:ELS:${groupId}`, bulletNum + '', { EX: 10 * 60 * 1000 })
