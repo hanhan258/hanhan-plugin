33abf79abfc0aa1fa329270985e9a2f2b1a4ba7e
bling-yshs <377178599@qq.com>
2024-02-08 17:18:02 +0800

    重写俄罗斯轮盘的判断逻辑，修复了剩余两发子弹时必中的BUG

--- apps/els.js
+++ apps/els.js
@@ -10,7 +10,7 @@ export class RussiaRoundPlatePlugin extends plugin {
       rule: [
         {
           reg: '^#?(开启俄罗斯轮盘|开盘|开启轮盘|开启转盘|俄罗斯轮盘)$',
-          fnc: 'start'
+          fnc: 'startELSGame'
         },
         {
           reg: '^#?开枪$',
@@ -18,7 +18,7 @@ export class RussiaRoundPlatePlugin extends plugin {
         },
         {
           reg: '^#?结束游戏$',
-          fnc: 'stopShoop'
+          fnc: 'stopELSGame'
         }, {
           reg: '^#?当前子弹$',
           fnc: 'nowBullet'
@@ -27,91 +27,98 @@ export class RussiaRoundPlatePlugin extends plugin {
     })
   }
 
-  async start (e) {
+  async startELSGame (e) {
     if (!e.isGroup) {
-      await e.reply('当前不在群聊里')
+      e.reply('当前不在群聊里')
       return false
     }
-
     let groupId = e.group_id
-    let groupLock = await redis.get(`HANHAN:ELS:${groupId}`)
-    if (!groupLock) {
-      let totalBullets = Math.floor(Math.random() * 6) + 3
-      let realBullets = Math.floor(Math.random() * totalBullets)
-      let bulletNum = JSON.stringify([totalBullets, realBullets])
-      await redis.set(`HANHAN:ELS:${groupId}`, bulletNum + '', { EX: 10 * 60 * 1000 })
-      await e.reply(`当前群俄罗斯轮盘已开启！\n弹夹有【${totalBullets}】发子弹。\n请发送#开枪 参与游戏`)
-    } else {
-      await e.reply('当前群俄罗斯轮盘正在进行中！\n请发送#开枪 参与游戏')
+    // 判断是否已经开启了俄罗斯轮盘
+    if (await redis.exists(`HANHAN:ELS2:${groupId}`) === 1) {
+      e.reply('当前群俄罗斯轮盘正在进行中！\n请发送#开枪 参与游戏')
+      return
     }
+    // 随机生成数组，长度从3到8，其中一项为子弹，剩下的为空(0为空，1为子弹)
+    let length = Math.floor(Math.random() * 6) + 3
+    // 随机指定数组的某一项为子弹，剩下的为空
+    let arr = new Array(length).fill(0)
+    let target = Math.floor(Math.random() * length)
+    arr[target] = 1
+    // 记录一下当前的数组
+    Bot.logger.mark('arr', arr)
+    await redis.set(`HANHAN:ELS2:${groupId}`, JSON.stringify(arr), { EX: 60 * 60 * 24 })
+    e.reply(`当前群俄罗斯轮盘已开启！\n弹夹有【${length}】发子弹。\n请发送#开枪 参与游戏`)
   }
 
   async shoot (e) {
     if (!e.isGroup) {
-      await e.reply('当前不在群聊里')
+      e.reply('当前不在群聊里')
       return false
     }
+    // 获取一下当前的用户昵称和群id
+    let username = e.sender.nickname
     let groupId = e.group_id
-    let leftBullets = JSON.parse(await redis.get(`HANHAN:ELS:${groupId}`))
-    if (!leftBullets) {
-      await this.start(e)
-      leftBullets = JSON.parse(await redis.get(`HANHAN:ELS:${groupId}`))
+    // 判断一下当前群是否开启了俄罗斯轮盘，如果没有开启则开启一下
+    if (await redis.exists(`HANHAN:ELS2:${groupId}`) === 0) {
+      await this.startELSGame(e)
     }
-    let username = e.sender.card || e.sender.nickname || (e.bot.config?.markdown ? `<@${e.sender.user_openid}>` : e.sender.user_openid)
-    let totalBullets = leftBullets[0]
-    let realBullets = leftBullets[1]
-    totalBullets--
-    realBullets--
-    if (realBullets < 1) {
-      let group = e.group || (await e.bot.pickGroup(groupId))
-      let max = 300
-      let min = 60
-      let time = Math.floor(Math.random() * (max - min + 1)) + min
-      let msg
-      try {
-        await group.muteMember(e.sender.user_id, time)
-        msg = `【${username}】开了一枪，枪响了。\n恭喜【${username}】被禁言${time}秒\n本轮游戏结束。请使用#开盘 开启新一轮游戏`
-      } catch (error) {
-        msg = `【${username}】开了一枪，枪响了。\n未获得管理员权限，跳过禁言环节。\n本轮游戏结束。请使用#开盘 开启新一轮游戏`
+    // 获取一下当前的数组
+    let arr = JSON.parse(await redis.get(`HANHAN:ELS2:${groupId}`))
+    // 处理一下高并发问题，如果当前的数组是null，则重新开启一下游戏
+    if (arr === null) {
+      await redis.del(`HANHAN:ELS2:${groupId}`)
+      await this.startELSGame(e)
+      return
+    }
+    if (arr[0] === 0) {
+      // 如果当前的数组第一项是0，则表示没响,弹出第一项
+      arr.shift()
+      // 检查一下是否只剩下一发子弹，如果是则结束游戏
+      if (arr.length === 1) {
+        e.reply([`【${username}】开了一枪，没响。\n由于只剩一发子弹，本轮游戏结束。\n请使用#开盘 开启新一轮游戏`, segment.image('https://www.loliapi.com/acg/pc/')])
+        await redis.del(`HANHAN:ELS2:${groupId}`)
+        return
       }
-      msg = (username != (e.sender.card | e.sender.nickname)) ? `【${username}】开了一枪，枪响了。\npia~\n本轮游戏结束。请使用#开盘 开启新一轮游戏` : msg
-      await e.reply(msg)
-      await redis.del(`HANHAN:ELS:${groupId}`)
-    } else {
-      leftBullets = JSON.stringify([totalBullets, realBullets])
-      await redis.set(`HANHAN:ELS:${groupId}`, leftBullets + '', { EX: 10 * 60 * 1000 })
-      await e.reply(`【${username}】开了一枪，没响。\n还剩【${totalBullets}】发子弹`)
-      // e.reply(`${leftBullets}`)
+      // 如果不是则继续游戏
+      e.reply(`【${username}】开了一枪，没响。\n还剩【${arr.length}】发子弹`)
+      await redis.set(`HANHAN:ELS2:${groupId}`, JSON.stringify(arr), { EX: 60 * 60 * 24 })
+      return
+    }
+    // 如果当前的数组第一项是1，则表示响了，结束游戏
+    if (arr[0] === 1) {
+      let time = Math.floor(Math.random() * 240) + 60
+      await e.group.muteMember(e.sender.user_id, time)
+      e.reply(`【${username}】开了一枪，枪响了。\n恭喜【${username}】被禁言${time}秒\n本轮游戏结束。请使用#开盘 开启新一轮游戏`)
+      await redis.del(`HANHAN:ELS2:${groupId}`)
     }
   }
 
-  async stopShoop (e) {
+  async stopELSGame (e) {
     if (!e.isGroup) {
-      await e.reply('当前不在群聊里')
+      e.reply('当前不在群聊里')
       return false
     }
     let groupId = e.group_id
-    let groupLock = await redis.get(`HANHAN:ELS:${groupId}`)
-    // e.reply(groupLock)
-    if (!groupLock) {
+    let arr = await redis.exists(`HANHAN:ELS2:${groupId}`)
+    if (arr === 0) {
       e.reply('当前群没有开盘')
     } else {
-      await redis.del(`HANHAN:ELS:${groupId}`)
+      await redis.del(`HANHAN:ELS2:${groupId}`)
       e.reply('结束成功')
     }
   }
 
   async nowBullet (e) {
     if (!e.isGroup) {
-      await e.reply('当前不在群聊里')
+      e.reply('当前不在群聊里')
       return false
     }
     let groupId = e.group_id
-    let groupLock = await redis.get(`HANHAN:ELS:${groupId}`)
-    if (!groupLock) {
+    let arr = JSON.parse(await redis.get(`HANHAN:ELS2:${groupId}`))
+    if (!arr) {
       e.reply('当前群没有开盘')
     } else {
-      e.reply('当前还有' + groupLock + '发子弹')
+      e.reply(`当前还有【${arr.length}】发子弹)`)
     }
   }
 }
