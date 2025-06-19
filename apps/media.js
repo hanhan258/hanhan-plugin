f07b42f624b9d645677ca1fa071dc31dbb533d58
ycxom <ycxom1@gmail.com>
2025-06-19 15:51:31 +0800

    feat: 添加#涩吗功能 - 修改请求头中的 User-Agent，使用自定义标识符

--- apps/media.js
+++ apps/media.js
@@ -622,7 +622,8 @@ export class media extends plugin {
                 ...options,
                 signal: controller.signal,
                 headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+                    'User-Agent': 'yunzai/hanhan-plugin',
+                    // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
                     ...options.headers
                 }
             })
